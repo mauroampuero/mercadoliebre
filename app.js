eab213186ecfa819a2e3fcd29bb370fbe02e8e01
MauroAmpuero <mau999ampu@gmail.com>
2021-07-29 19:48:42 -0300

    listo

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+app.get('/', function(req,res){res.send('Hola Mundo!')
+
+})
+
+app.get('/home', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/index.html'))})
+app.get('/login', function(req,res){res.sendFile(path.join(__dirname, 'views/login.html'))})
+app.get('/register', function(req,res){res.sendFile(path.join(__dirname, 'views/register.html'))})
+
+app.use(express.static('public'))
+
+app.get('*', function(req,res){
+    res.send('NOT FOUND')
+})
+
+app.listen(8000, ()=> {
+    console.log('Pagina corriendo en http://localhost:8000')
+
+})
+
